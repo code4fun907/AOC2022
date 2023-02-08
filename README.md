2d6d7596a92003888f2c39ce7b44c212e51fa782
Skyler Brandt <skylerak90704@gmail.com>
2023-02-07 23:31:33 -0900

    Scaffold Day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+This advent of code I will be doing every day in a different programming language
+
+Day 1 - Clojure
---
+++ day1/day1.clj
@@ -0,0 +1,8 @@
+(defn day-one-part-one []
+    (println "not implemented"))
+
+(defn day-one-part-two []
+    (println "not implemented"))
+
+(day-one-part-one)
+(day-one-part-two)
